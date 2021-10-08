1a398a01352b042b76b8962f7c19e036483a5dcc
Jiutian Zhang <jzhang3211@gatech.edu>
2021-10-08 17:33:28 -0300

    Question 3

--- tests/functional/people/test_get_people.py
+++ tests/functional/people/test_get_people.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from tests.factories import PersonFactory
+from tests.factories import ConnectionFactory, PersonFactory
 
 EXPECTED_FIELDS = [
     'id',
@@ -22,3 +22,33 @@ def test_can_get_people(db, testapp):
     for person in res.json:
         for field in EXPECTED_FIELDS:
             assert field in person
+
+
+def test_can_get_mutual_friends(db, testapp):
+    instance = PersonFactory()
+    target = PersonFactory()
+
+    # some decoy connections (not mutual)
+    ConnectionFactory.create_batch(5, to_person=instance)
+    ConnectionFactory.create_batch(5, to_person=target)
+
+    mutual_friends = PersonFactory.create_batch(3)
+    for f in mutual_friends:
+        ConnectionFactory(from_person=instance, to_person=f, connection_type='friend')
+        ConnectionFactory(from_person=target, to_person=f, connection_type='friend')
+
+    # mutual connections, but not friends
+    decoy = PersonFactory()
+    ConnectionFactory(from_person=instance, to_person=decoy, connection_type='coworker')
+    ConnectionFactory(from_person=target, to_person=decoy, connection_type='coworker')
+
+    db.session.commit()
+    res = testapp.get('/people/{person_id}/mutual_friends?target_id={target_id}'.format(
+        person_id=instance.id,
+        target_id=target.id,
+    ))
+    assert len(res.json) == len(mutual_friends)
+    assert res.status_code == HTTPStatus.OK
+    expected_mutual_friend_ids = [f.id for f in mutual_friends]
+    actual_mutual_friend_ids = [p['id'] for p in res.json]
+    assert actual_mutual_friend_ids == expected_mutual_friend_ids
